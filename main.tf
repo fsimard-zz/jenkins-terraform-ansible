10581ac4f75517adfa94ae8fc8b1fbd0584b56cc
lmoreault <luc.moreault@bnc.ca>
2019-06-11 12:05:57 -0400

    Small fix for port

--- main.tf
+++ main.tf
@@ -88,8 +88,8 @@ resource "aws_security_group" "allow_http" {
   name   = "allow_http"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
